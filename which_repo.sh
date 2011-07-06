3140e1c2c2675475c046354a275b6d32239bfb07
Andrew Ray <delvarworld@gmail.com>
2011-07-06 11:40:21 +1000

    faster which_repo file

--- which_repo.sh
+++ which_repo.sh
@@ -1,13 +1,21 @@
 IS_GIT_DIR=false
 IS_HG_DIR=false
+IS_SVN_DIR=false
 
 gitTest=`git status 2> /dev/null`
-hgTest=`hg status 2> /dev/null`
-
 if [[ "$gitTest" != "" ]]; then
     IS_GIT_DIR=true
-fi
 
-if [[ "$hgTest" != "" ]]; then
-    IS_HG_DIR=true
+else 
+    hgTest=`hg summary 2> /dev/null`
+    if [[ "$hgTest" != "" ]]; then
+        IS_HG_DIR=true
+    else
+        svnTest=`svn info 2> /dev/null`
+        if [[ "$svnTest" != "" ]]; then
+            IS_SVN_DIR=true
+        else
+            echo "This is not a Git, Mercurial nor Subversion repository. Are you drunk?"
+        fi
+    fi
 fi
